03b27071e3260f8261fdf65a1d199d2ab66600ed
oskarbringle <osar.bringle@gmail.com>
2024-09-15 14:46:58 -0400

    Building frontend w/ React and Chakra-ui
    Building the frontend using react as well as chakra components. Still in progress, but this is the shell

---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    <ChakraProvider>
+      <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </StrictMode>,
+)
